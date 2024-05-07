a674c8c187c48d927aa5200819f4602f10ebbe90
SHUBHAM KUMAR <shubham.rajveer19@gmail.com>
2024-05-07 20:25:55 +0530

    changes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -15,40 +15,35 @@ import { useAuth } from "@clerk/clerk-react";
 // type Props = {};
 
 const Login = () => {
-  const { userId } = useAuth();
-  const urlParams = new URLSearchParams(window.location.search);
-  const redirect = urlParams.get("redirect") ?? "";
-  const url = window.location.origin + redirect;
 
+  const { userId } = useAuth();
+    const urlParams = new URLSearchParams(window.location.search);
+    const redirect = urlParams.get("redirect") ??"";
+    const url = window.location.origin + redirect;
+    console.log("redirect", url);
   return (
-    <div className="flex gap-2 w-screen h-screen bg-black">
-      {/* Background Images */}
+    <div className="flex  gap-2 w-screen h-screen bg-black">
       <img
         src={SideImg}
         alt="sideImg"
-        className="hidden lg:block object-cover w-1/2 h-full drop-shadow-lg"
+        className=" object-cover lg:flex w-screen h-screen drop-shadow-lg"
       />
       <img
         src={One}
         alt="sideImg"
-        className="hidden lg:block absolute left-0 bottom-0 mix-blend-exclusion"
+        className="absolute hidden lg:flex left-0 bottom-0  mix-blend-exclusion"
       />
       <img
         src={Two}
         alt="sideImg"
-        className="hidden lg:block absolute right-0 top-0 mix-blend-exclusion"
+        className="absolute hidden lg:flex right-0 top-0 mix-blend-exclusion"
       />
-
-      <div className="hidden lg:flex justify-center items-center w-1/2 h-screen">
-        <div className="absolute rounded-lg top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 p-4">
-          <SignIn redirectUrl={redirect} />
-          
-        </div>
-
-      
+ 
+      <div className="absolute bg-white rounded-lg top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2 items-center gap-4 cursor-pointer">
+        <SignIn redirectUrl={redirect} />
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Login;
