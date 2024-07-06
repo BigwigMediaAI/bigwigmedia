9b703571fd33a0ebfd77a1e7c6068aea737af169
SHUBHAM KUMAR <shubham.rajveer19@gmail.com>
2024-07-06 15:13:40 +0530

    custom plan

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -9,17 +9,17 @@ const PricingPlan = () => {
   const handleChoosePlan = () => {
     navigate('/plan');
   };
-  // const handleChooseFree = () => {
-  //   navigate('/login');
-  // };
+  const handleContact = () => {
+    navigate('/contact');
+  };
 
   return (
     <div className='mt-10'>
     <BlogTitle>Pricing Plan</BlogTitle>
     <div className="flex flex-col  items-center py-6 ">
-      <div className="flex flex-col md:flex-row gap-20">
+      <div className="flex flex-col md:flex-row gap-10">
          {/* Free Plan */}
-      <div className="max-w-md  rounded-lg shadow-lg p-6 shadow-violet-700">
+      <div className="max-w-sm  rounded-lg shadow-lg p-6 shadow-violet-700">
         <h3 className="text-xl font-semibold text-gray-200 mb-4">Free Plan</h3>
         <div className="text-4xl font-bold text-gray-300 mb-4">₹0</div>
         <div className="text-gray-400 mb-4">30 credits for 7 days</div>
@@ -42,13 +42,7 @@ const PricingPlan = () => {
           </svg>
           <span className="text-gray-400">Easy Audio Transcriptions</span>
         </div>
-        <div className="flex items-center mb-4">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M4 4a1 1 0 0 1 2 0v6a1 1 0 0 1-2 0V4zm12 0a1 1 0 0 0-2 0v6a1 1 0 0 0 2 0V4zM5 10a1 1 0 0 1-2 0V4a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v6a1 1 0 1 1-2 0V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v6zM9 20a1 1 0 0 1-1-1v-3.585a1 1 0 0 1 .293-.707l6.292-6.293a1 1 0 0 1 1.414 0l3.585 3.585a1 1 0 0 1 0 1.414l-6.292 6.292a1 1 0 0 1-.707.293H9z" />
-          </svg>
-          <span className="text-gray-400">Generate Text-to-Speech
-          </span>
-        </div>
+        
         <div className="flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M4 4a1 1 0 0 1 2 0v6a1 1 0 0 1-2 0V4zm12 0a1 1 0 0 0-2 0v6a1 1 0 0 0 2 0V4zM5 10a1 1 0 0 1-2 0V4a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v6a1 1 0 1 1-2 0V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v6zM9 20a1 1 0 0 1-1-1v-3.585a1 1 0 0 1 .293-.707l6.292-6.293a1 1 0 0 1 1.414 0l3.585 3.585a1 1 0 0 1 0 1.414l-6.292 6.292a1 1 0 0 1-.707.293H9z" />
@@ -62,14 +56,14 @@ const PricingPlan = () => {
           <span className="text-gray-400">35+ languages</span>
         </div>
         <button
-          className="mt-5 text-white text-center font-outfit md:text-lg font-semibold flex  text-xs py-3 px-10 justify-center items-center gap-4 flex-shrink-0 rounded-full bt-gradient hover:opacity-80 w-fit mx-auto"
+          className="mt-5 text-white text-center font-outfit md:text-lg font-semibold flex  text-xs py-2 px-6 justify-center items-center gap-4 flex-shrink-0 rounded-full bt-gradient hover:opacity-80 w-fit mx-auto"
           onClick={handleChoosePlan}
         >
           Choose Plan
         </button>
       </div>
       {/* Monthly Plan */}
-      <div className="max-w-md  rounded-lg shadow-lg p-6 shadow-violet-700">
+      <div className="max-w-sm  rounded-lg shadow-lg p-6 shadow-violet-700">
         <h3 className="text-xl font-semibold text-gray-200 mb-4">Monthly Plan</h3>
         <div className="text-4xl font-bold text-gray-300 mb-4">₹1000</div>
         <div className="text-gray-400 mb-4">300 credits for 30 days</div>
@@ -91,13 +85,7 @@ const PricingPlan = () => {
           </svg>
           <span className="text-gray-400">Easy Audio Transcriptions</span>
         </div>
-        <div className="flex items-center mb-4">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M4 4a1 1 0 0 1 2 0v6a1 1 0 0 1-2 0V4zm12 0a1 1 0 0 0-2 0v6a1 1 0 0 0 2 0V4zM5 10a1 1 0 0 1-2 0V4a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v6a1 1 0 1 1-2 0V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v6zM9 20a1 1 0 0 1-1-1v-3.585a1 1 0 0 1 .293-.707l6.292-6.293a1 1 0 0 1 1.414 0l3.585 3.585a1 1 0 0 1 0 1.414l-6.292 6.292a1 1 0 0 1-.707.293H9z" />
-          </svg>
-          <span className="text-gray-400">Generate Text-to-Speech
-          </span>
-        </div>
+        
         <div className="flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M4 4a1 1 0 0 1 2 0v6a1 1 0 0 1-2 0V4zm12 0a1 1 0 0 0-2 0v6a1 1 0 0 0 2 0V4zM5 10a1 1 0 0 1-2 0V4a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v6a1 1 0 1 1-2 0V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v6zM9 20a1 1 0 0 1-1-1v-3.585a1 1 0 0 1 .293-.707l6.292-6.293a1 1 0 0 1 1.414 0l3.585 3.585a1 1 0 0 1 0 1.414l-6.292 6.292a1 1 0 0 1-.707.293H9z" />
@@ -111,24 +99,69 @@ const PricingPlan = () => {
           <span className="text-gray-400">35+ languages</span>
         </div>
         <button
-          className="mt-5 text-white text-center font-outfit md:text-lg font-semibold flex  text-xs py-3 px-10 justify-center items-center gap-4 flex-shrink-0 rounded-full bt-gradient hover:opacity-80 w-fit mx-auto"
+          className="mt-5 text-white text-center font-outfit md:text-lg font-semibold flex  text-xs py-2 px-6 justify-center items-center gap-4 flex-shrink-0 rounded-full bt-gradient hover:opacity-80 w-fit mx-auto"
           onClick={handleChoosePlan}
         >
           Choose Plan
         </button>
       </div>
 
- {/* Yearly Plan */}
- <div className="max-w-md rounded-lg shadow-lg p-6 shadow-violet-700">
-        <h3 className="text-xl font-semibold text-gray-200 mb-4">Yearly Plan</h3>
-        <div className="text-4xl font-bold text-gray-300 mb-4">₹10000</div>
-        <div className="text-gray-400 mb-4">4000 credits for 365 days</div>
-        
+      {/* Yearly Plan */}
+      <div className="max-w-sm rounded-lg shadow-lg p-6 shadow-violet-700">
+              <h3 className="text-xl font-semibold text-gray-200 mb-4">Yearly Plan</h3>
+              <div className="text-4xl font-bold text-gray-300 mb-4">₹10000</div>
+              <div className="text-gray-400 mb-4">4000 credits for 365 days</div>
+              
+              <div className="flex items-center mb-4">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M4 4a1 1 0 0 1 2 0v6a1 1 0 0 1-2 0V4zm12 0a1 1 0 0 0-2 0v6a1 1 0 0 0 2 0V4zM5 10a1 1 0 0 1-2 0V4a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v6a1 1 0 1 1-2 0V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v6zM9 20a1 1 0 0 1-1-1v-3.585a1 1 0 0 1 .293-.707l6.292-6.293a1 1 0 0 1 1.414 0l3.585 3.585a1 1 0 0 1 0 1.414l-6.292 6.292a1 1 0 0 1-.707.293H9z" />
+                </svg>
+                <span className="text-gray-400">Use GPT-4</span>
+              </div>
+              <div className="flex items-center mb-4">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M4 4a1 1 0 0 1 2 0v6a1 1 0 0 1-2 0V4zm12 0a1 1 0 0 0-2 0v6a1 1 0 0 0 2 0V4zM5 10a1 1 0 0 1-2 0V4a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v6a1 1 0 1 1-2 0V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v6zM9 20a1 1 0 0 1-1-1v-3.585a1 1 0 0 1 .293-.707l6.292-6.293a1 1 0 0 1 1.414 0l3.585 3.585a1 1 0 0 1 0 1.414l-6.292 6.292a1 1 0 0 1-.707.293H9z" />
+                </svg>
+                <span className="text-gray-400">Dall-e-3 Image Generation</span>
+              </div>
+              <div className="flex items-center mb-4">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M4 4a1 1 0 0 1 2 0v6a1 1 0 0 1-2 0V4zm12 0a1 1 0 0 0-2 0v6a1 1 0 0 0 2 0V4zM5 10a1 1 0 0 1-2 0V4a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v6a1 1 0 1 1-2 0V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v6zM9 20a1 1 0 0 1-1-1v-3.585a1 1 0 0 1 .293-.707l6.292-6.293a1 1 0 0 1 1.414 0l3.585 3.585a1 1 0 0 1 0 1.414l-6.292 6.292a1 1 0 0 1-.707.293H9z" />
+                </svg>
+                <span className="text-gray-400">Easy Audio Transcriptions</span>
+              </div>
+              
+              <div className="flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M4 4a1 1 0 0 1 2 0v6a1 1 0 0 1-2 0V4zm12 0a1 1 0 0 0-2 0v6a1 1 0 0 0 2 0V4zM5 10a1 1 0 0 1-2 0V4a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v6a1 1 0 1 1-2 0V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v6zM9 20a1 1 0 0 1-1-1v-3.585a1 1 0 0 1 .293-.707l6.292-6.293a1 1 0 0 1 1.414 0l3.585 3.585a1 1 0 0 1 0 1.414l-6.292 6.292a1 1 0 0 1-.707.293H9z" />
+                </svg>
+                <span className="text-gray-400">120+ Tools</span>
+              </div>
+              <div className="flex items-center mt-4">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M4 4a1 1 0 0 1 2 0v6a1 1 0 0 1-2 0V4zm12 0a1 1 0 0 0-2 0v6a1 1 0 0 0 2 0V4zM5 10a1 1 0 0 1-2 0V4a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v6a1 1 0 1 1-2 0V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v6zM9 20a1 1 0 0 1-1-1v-3.585a1 1 0 0 1 .293-.707l6.292-6.293a1 1 0 0 1 1.414 0l3.585 3.585a1 1 0 0 1 0 1.414l-6.292 6.292a1 1 0 0 1-.707.293H9z" />
+                </svg>
+                <span className="text-gray-400">35+ languages</span>
+              </div>
+              <button
+                className="mt-5 text-white text-center font-outfit md:text-lg font-semibold flex  text-xs py-2 px-6 justify-center items-center gap-4 flex-shrink-0 rounded-full bt-gradient hover:opacity-80 w-fit mx-auto"
+                onClick={handleChoosePlan}
+              >
+                Choose Plan
+              </button>
+      </div>
+
+      {/* Custom plan */}
+
+      <div className="max-w-unit-64  rounded-lg shadow-lg p-6 shadow-violet-700">
+        <h3 className="text-xl font-semibold text-gray-200 mb-4 ">Custom Plan</h3>
+        <div className="text-gray-400 mb-4">
+        Contact us directly to request credits and get pricing based on your needs.</div><br />
         <div className="flex items-center mb-4">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M4 4a1 1 0 0 1 2 0v6a1 1 0 0 1-2 0V4zm12 0a1 1 0 0 0-2 0v6a1 1 0 0 0 2 0V4zM5 10a1 1 0 0 1-2 0V4a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v6a1 1 0 1 1-2 0V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v6zM9 20a1 1 0 0 1-1-1v-3.585a1 1 0 0 1 .293-.707l6.292-6.293a1 1 0 0 1 1.414 0l3.585 3.585a1 1 0 0 1 0 1.414l-6.292 6.292a1 1 0 0 1-.707.293H9z" />
           </svg>
-          <span className="text-gray-400">Use GPT-4</span>
+          <span className="text-gray-400">Use GPT-4 </span>
         </div>
         <div className="flex items-center mb-4">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
@@ -136,19 +169,8 @@ const PricingPlan = () => {
           </svg>
           <span className="text-gray-400">Dall-e-3 Image Generation</span>
         </div>
-        <div className="flex items-center mb-4">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M4 4a1 1 0 0 1 2 0v6a1 1 0 0 1-2 0V4zm12 0a1 1 0 0 0-2 0v6a1 1 0 0 0 2 0V4zM5 10a1 1 0 0 1-2 0V4a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v6a1 1 0 1 1-2 0V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v6zM9 20a1 1 0 0 1-1-1v-3.585a1 1 0 0 1 .293-.707l6.292-6.293a1 1 0 0 1 1.414 0l3.585 3.585a1 1 0 0 1 0 1.414l-6.292 6.292a1 1 0 0 1-.707.293H9z" />
-          </svg>
-          <span className="text-gray-400">Easy Audio Transcriptions</span>
-        </div>
-        <div className="flex items-center mb-4">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M4 4a1 1 0 0 1 2 0v6a1 1 0 0 1-2 0V4zm12 0a1 1 0 0 0-2 0v6a1 1 0 0 0 2 0V4zM5 10a1 1 0 0 1-2 0V4a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v6a1 1 0 1 1-2 0V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v6zM9 20a1 1 0 0 1-1-1v-3.585a1 1 0 0 1 .293-.707l6.292-6.293a1 1 0 0 1 1.414 0l3.585 3.585a1 1 0 0 1 0 1.414l-6.292 6.292a1 1 0 0 1-.707.293H9z" />
-          </svg>
-          <span className="text-gray-400">Generate Text-to-Speech
-          </span>
-        </div>
+        
+        
         <div className="flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M4 4a1 1 0 0 1 2 0v6a1 1 0 0 1-2 0V4zm12 0a1 1 0 0 0-2 0v6a1 1 0 0 0 2 0V4zM5 10a1 1 0 0 1-2 0V4a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v6a1 1 0 1 1-2 0V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v6zM9 20a1 1 0 0 1-1-1v-3.585a1 1 0 0 1 .293-.707l6.292-6.293a1 1 0 0 1 1.414 0l3.585 3.585a1 1 0 0 1 0 1.414l-6.292 6.292a1 1 0 0 1-.707.293H9z" />
@@ -162,12 +184,13 @@ const PricingPlan = () => {
           <span className="text-gray-400">35+ languages</span>
         </div>
         <button
-          className="mt-5 text-white text-center font-outfit md:text-lg font-semibold flex  text-xs py-3 px-10 justify-center items-center gap-4 flex-shrink-0 rounded-full bt-gradient hover:opacity-80 w-fit mx-auto"
-          onClick={handleChoosePlan}
+          className="mt-5 text-white text-center font-outfit md:text-lg font-semibold flex  text-xs py-2 px-8 justify-center items-center gap-4 flex-shrink-0 rounded-full bt-gradient hover:opacity-80 w-fit mx-auto"
+          onClick={handleContact}
         >
-          Choose Plan
+          Contact us
         </button>
       </div>
+
       </div>
 
       {/* Safe Payment Section */}
