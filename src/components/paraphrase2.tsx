b68ccfa62bd9de576e51bb35cbd7b0967b07745b
chandan2812 <chandanpms@gmail.com>
2024-08-16 16:06:54 +0530

    done

--- src/components/paraphrase2.tsx
+++ src/components/paraphrase2.tsx
@@ -4,26 +4,33 @@ import { Textarea } from "@/components/ui/textarea";
 import { BASE_URL } from "@/utils/funcitons";
 import { useAuth } from "@clerk/clerk-react";
 import axios from "axios";
-import { Loader2 } from "lucide-react";
-import { useState,useRef, useEffect } from "react";
+import { Copy, Download, Loader2, Share2 } from "lucide-react";
+import { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import { Element } from "./Generate2";
 
-export function Script
-  ({ groups,val,setVal,output,handleSubmit,isLoading }: {
-    groups: any, val: any;
-    setVal: Function;
-    output:any;
-    handleSubmit:Function;
-    isLoading:boolean;
+export function Script({
+  groups,
+  val,
+  setVal,
+  output,
+  handleSubmit,
+  isLoading,
+}: {
+  groups: any;
+  val: any;
+  setVal: Function;
+  output: any;
+  handleSubmit: Function;
+  isLoading: boolean;
 }) {
   const [text, setText] = useState("");
   const navigate = useNavigate();
   const loaderRef = useRef<HTMLDivElement>(null);
   const resultsRef = useRef<HTMLDivElement>(null);
 
-  console.log(groups)
+  console.log(groups);
 
   const handlePaste = async () => {
     const text = await navigator.clipboard.readText();
@@ -37,9 +44,12 @@ export function Script
       const outputElement = document.querySelector('.output-div');
       if (outputElement) {
         const outputText = outputElement.textContent || '';
-        console.log(outputText)
-        navigator.clipboard.writeText(outputText);
-        toast.success("Copied to Clipboard");
+        console.log(outputText);
+        navigator.clipboard.writeText(outputText).then(() => {
+          toast.success("Copied to Clipboard");
+        }).catch(() => {
+          toast.error("Failed to copy");
+        });
       } else {
         toast.error("Output element not found");
       }
@@ -96,15 +106,20 @@ export function Script
   const handleCopyEvent = (e: ClipboardEvent) => {
     const selectedText = window.getSelection()?.toString() || '';
     if (selectedText) {
-        e.clipboardData?.setData('text/plain', selectedText);
-        e.preventDefault();
+      e.clipboardData?.setData('text/plain', selectedText);
+      e.preventDefault();
     }
-};
+  };
 
-document.addEventListener('copy', handleCopyEvent);
+  useEffect(() => {
+    document.addEventListener('copy', handleCopyEvent);
+    return () => {
+      document.removeEventListener('copy', handleCopyEvent);
+    };
+  }, []);
 
   return (
-    <div className="m-auto w-full max-w-4xl rounded-lg dark:bg-zinc-900 bg-white p-6 shadow-lg">
+    <div className="m-auto w-full max-w-4xl rounded-lg bg-[var(--white-color)] p-6 shadow-md shadow-[var(--teal-color)]">
       <div className="flex flex-col ">
         <div className="w-full pr-2">
           <div className="flex justify-center px-5 max-w-[1084px] w-full mx-auto items-center flex-col gap-8">
@@ -121,7 +136,7 @@ document.addEventListener('copy', handleCopyEvent);
           </div>
           <div className="flex w-full my-4 items-center justify-center">
             <Button
-              className="text-white text-center font-outfit md:text-lg font-semibold flex relative text-base py-3 px-10 justify-center items-center gap-4 flex-shrink-0 rounded-full bt-gradient disabled:opacity-60 hover:opacity-80 w-fit mx-auto"
+              className="text-white text-center font-outfit md:text-lg font-semibold flex relative text-base py-3 px-10 justify-center items-center gap-4 flex-shrink-0 rounded-full bg-[var(--teal-color)] disabled:opacity-60 hover:bg-[var(--hover-teal-color)] w-fit mx-auto"
               onClick={(e) => void handleSubmit(e)}
             >
               {isLoading}
@@ -131,113 +146,53 @@ document.addEventListener('copy', handleCopyEvent);
         </div>
         <div className="w-full pl-2 flex flex-col gap-2 justify-between">
           {isLoading ? (
-            <div ref={loaderRef} className="w-full h-full flex-col items-center justify-center">
-              <Loader2 className="animate-spin w-20 h-20 mt-20" />
-              <p className="text-gray-300 text-justify">Data processing in progress. Please bear with us...</p>
+            <div ref={loaderRef} className="w-full flex flex-col items-center justify-center">
+              <Loader2 className="animate-spin w-20 h-20 mt-5 text-[var(--dark-gray-color)]" />
+              <p className="text-[var(--dark-gray-color)] text-justify">
+                Data processing in progress. Please bear with us...
+              </p>
             </div>
           ) : (
-            <div ref={resultsRef} className=" w-full rounded-md  dark:text-gray-200 text-gray-800 p-5 ">
+            <div ref={resultsRef} className="w-full rounded-md text-gray-800 p-5">
+              {!!output?.output && (
+                <div className="flex items-end gap-2">
+                  <Button
+                    className="text-[var(--primary-text-color)] hover:text-[var(--hover-teal-color)] cursor-pointer"
+                    variant="ghost"
+                    onClick={handleCopy}
+                    disabled={isLoading}
+                    title="Copy"
+                  >
+                    <Copy className="mr-2 h-5 w-5" />
+                  </Button>
+                  <Button
+                    className="text-[var(--primary-text-color)] hover:text-[var(--hover-teal-color)] cursor-pointer"
+                    variant="ghost"
+                    onClick={handleShare}
+                    disabled={isLoading}
+                    title="Share"
+                  >
+                    <Share2 className="mr-2 h-5 w-5" />
+                  </Button>
+                  <Button
+                    className="text-[var(--primary-text-color)] hover:text-[var(--hover-teal-color)] cursor-pointer"
+                    variant="ghost"
+                    onClick={handleDownload}
+                    disabled={isLoading}
+                    title="Download"
+                  >
+                    <Download className="mr-2 h-5 w-5" />
+                  </Button>
+                </div>
+              )}
               <p
-                className="p-5 text-base md:text-xl font-medium output-div"
+                className="p-5 text-base md:text-xl font-medium output-div whitespace-pre-wrap"
                 dangerouslySetInnerHTML={{ __html: output?.output as string }}
               />
-            </div>
-          )}
-          {!!output?.output && (
-            <div className="flex gap-2">
-              <Button
-                className="rounded-md mb-6 px-4 py-0 text-gray-600 hover:dark:bg-gray-800 disabled:opacity-90 opacity-100 flex gap-2 dark:text-gray-200 hover:bg-gray-100"
-                variant="ghost"
-                onClick={handleCopy}
-                disabled={isLoading}
-              title="Share">
-                <CopyIcon className="mr-2 h-5 w-5" />
-                Copy
-              </Button>
-              <Button
-                className="rounded-md mb-6 px-4 py-0 text-gray-600 hover:dark:bg-gray-800 disabled:opacity-90 opacity-100 flex gap-2 dark:text-gray-200 hover:bg-gray-100"
-                variant="ghost"
-                onClick={handleShare}
-                disabled={isLoading}
-              title="Share">
-                <ShareIcon className="mr-2 h-5 w-5" />
-                Share
-              </Button>
-              <Button
-                className="rounded-md mb-6 px-4 py-0 text-gray-600 hover:dark:bg-gray-800 disabled:opacity-90 opacity-100 flex gap-2 dark:text-gray-200 hover:bg-gray-100"
-                variant="ghost"
-                onClick={handleDownload}
-                disabled={isLoading}
-              title="Download">
-                <DownloadIcon className="mr-2 h-5 w-5" />
-                Download
-              </Button>
             </div>
           )}
         </div>
       </div>
     </div>
-  );
-}
-
-function CopyIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
-      <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
-    </svg>
-  );
-}
-
-function ShareIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M4 12v2a2 2 0 0 0 2 2h3l2 2h-7a4 4 0 0 1-4-4v-2" />
-      <path d="M20 8V6a2 2 0 0 0-2-2h-3l-2-2h7a4 4 0 0 1 4 4v2" />
-      <line x1="12" y1="12" x2="12" y2="12" />
-    </svg>
-  );
-}
-
-function DownloadIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M8 17l4 4 4-4" />
-      <line x1="12" y1="12" x2="12" y2="21" />
-      <path d="M20 16v-5a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v5" />
-    </svg>
   );
 }
