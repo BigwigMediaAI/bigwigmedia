c20e280ff32d14f6a5326368ce36ab21f3257e5f
chandan2812 <chandanpms@gmail.com>
2024-08-19 13:53:27 +0530

    done

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -9,31 +9,31 @@ const Stats: React.FC = () => {
           <div className="text-center flex flex-col justify-center items-center">
             <div className="flex items-center gap-2">
               <FaMedal className="text-xl text-[var(--teal-color)]" />
-              <p className="text-lg sm:text-md lg:text-xl text-[var(--teal-color)] font-outfit font-semibold">No.1</p>
+              <p className="text-lg sm:text-md lg:text-xl text-[var(--teal-color)] font-outfit font-bold">No.1</p>
             </div>
             <p className="text-sm sm:text-md lg:text-lg text-[var(--dark-gray-color)]">Product</p>
           </div>
           <div className="text-center flex flex-col justify-center items-center">
             <div className="flex items-center gap-2">
               <FaGlobe className="text-xl text-[var(--teal-color)]" />
-              <p className="text-lg sm:text-md lg:text-xl text-[var(--teal-color)] font-outfit font-semibold">20+</p>
+              <p className="text-lg sm:text-md lg:text-xl text-[var(--teal-color)] font-outfit font-bold">20+</p>
             </div>
             <p className="text-sm sm:text-md lg:text-lg text-[var(--dark-gray-color)]">Countries</p>
           </div>
           <div className="text-center flex flex-col justify-center items-center">
-            <p className="text-lg sm:text-md lg:text-xl text-[var(--teal-color)] font-outfit font-semibold">1.4M+</p>
+            <p className="text-lg sm:text-md lg:text-xl text-[var(--teal-color)] font-outfit font-bold">1.4M+</p>
             <p className="text-sm sm:text-md lg:text-lg text-[var(--dark-gray-color)]">Happy Users</p>
           </div>
           <div className="text-center flex flex-col justify-center items-center">
-            <p className="text-lg sm:text-md lg:text-xl text-[var(--teal-color)] font-outfit font-semibold">8.5M+</p>
+            <p className="text-lg sm:text-md lg:text-xl text-[var(--teal-color)] font-outfit font-bold">8.5M+</p>
             <p className="text-sm sm:text-md lg:text-lg text-[var(--dark-gray-color)]">Files Converted</p>
           </div>
           <div className="text-center flex flex-col justify-center items-center">
-            <p className="text-lg sm:text-md lg:text-xl text-[var(--teal-color)] font-outfit font-semibold">150+</p>
+            <p className="text-lg sm:text-md lg:text-xl text-[var(--teal-color)] font-outfit font-bold">150+</p>
             <p className="text-sm sm:text-md lg:text-lg text-[var(--dark-gray-color)]">Online Tools</p>
           </div>
           <div className="text-center flex flex-col justify-center items-center">
-            <p className="text-lg sm:text-md lg:text-xl text-[var(--teal-color)] font-outfit font-semibold">12.6M+</p>
+            <p className="text-lg sm:text-md lg:text-xl text-[var(--teal-color)] font-outfit font-bold">12.6M+</p>
             <p className="text-sm sm:text-md lg:text-lg text-[var(--dark-gray-color)]">Content Generated</p>
           </div>
         </div>
