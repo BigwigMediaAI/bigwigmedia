00837cec3412bddd0a966bb674c632915e1b5ba2
chandan2812 <chandanpms@gmail.com>
2024-10-09 17:04:31 +0530

    updated

--- src/components/PdfMerge.tsx
+++ src/components/PdfMerge.tsx
@@ -185,7 +185,7 @@ export function PDFMerger() {
         ) : (
           pdfUrl && (
             <div ref={resultsRef} className="mt-5 text-center">
-              <iframe src={pdfUrl} width="100%" height="500px"></iframe>
+              <embed src={pdfUrl} width="100%" height="500px" type="application/pdf" />
               <Button
                 className="mt-5 text-white text-center font-outfit md:text-lg font-semibold flex relative text-base py-7 px-10 justify-center items-center gap-4 flex-shrink-0 rounded-full bg-[var(--teal-color)] disabled:opacity-60 hover:bg-[var(--hover-teal-color)] w-fit mx-auto"
                 onClick={() => {
