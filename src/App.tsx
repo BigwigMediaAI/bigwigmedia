1660890ec1bc1071ec166ba83950e19bb730707d
chandan2812 <chandanpms@gmail.com>
2024-10-23 12:58:56 +0530

    google ad content

--- src/App.tsx
+++ src/App.tsx
@@ -59,6 +59,7 @@ import { YoutubeScriptOutline } from "./components/YoutubeScriptOutline";
 import { TiktokCaption } from "./components/TiktokCaption";
 import { AboutmeGenerator } from "./components/AboutmeGenerator";
 import { PodcastSummary } from "./components/PodcastSummaryGenerator";
+import { GoogleAdContent } from "./components/GoogleAdContent";
 
 const App = () => {
  
@@ -130,6 +131,7 @@ const App = () => {
         <Route path="/caption" element={<TiktokCaption />} />   
         <Route path="/aboutme" element={<AboutmeGenerator />} />
         <Route path="/pod" element={<PodcastSummary />} />
+        <Route path="/ad" element={<GoogleAdContent />} />
 </Routes>
     </div>
   );
