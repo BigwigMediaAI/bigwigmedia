7ba6e1aea4775f29d13f609db5d6d70062c964cd
chandan2812 <chandanpms@gmail.com>
2024-11-05 15:01:24 +0530

    blog post introduction

--- src/App.tsx
+++ src/App.tsx
@@ -59,6 +59,7 @@ import { YoutubeScriptOutline } from "./components/YoutubeScriptOutline";
 import { TiktokCaption } from "./components/TiktokCaption";
 import { AboutmeGenerator } from "./components/AboutmeGenerator";
 import { PodcastSummary } from "./components/PodcastSummaryGenerator";
+import { BlogPostIntro } from "./components/BlogPostIntroduction";
 
 const App = () => {
  
@@ -130,6 +131,7 @@ const App = () => {
         <Route path="/caption" element={<TiktokCaption />} />   
         <Route path="/aboutme" element={<AboutmeGenerator />} />
         <Route path="/pod" element={<PodcastSummary />} />
+        <Route path="/blogpost" element={<BlogPostIntro />} />
 </Routes>
     </div>
   );
